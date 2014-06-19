25e82c23b68e0f8322b0ee3e1c077fc8f2a20f54
Ivan Bahdanau <ibahdanau@fitbit.com>
2014-06-19 13:46:30 -0700

    minor - rest service returns fake valid data now.

--- src/main/java/com/fitbit/oauth/RestService.java
+++ src/main/java/com/fitbit/oauth/RestService.java
@@ -1,7 +1,6 @@
 package com.fitbit.oauth;
 
 import com.fitbit.oauth.error.AuthException;
-import com.fitbit.oauth.model.RequestResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -23,19 +22,19 @@ public class RestService {
     @Autowired
     private AuthService authService;
 
-    @RequestMapping(value = "authorize", produces = {"application/json"})
+    @RequestMapping(value = "authorize")
     @ResponseStatus(HttpStatus.OK)
     public
     @ResponseBody
-    RequestResult sayHello(HttpServletRequest request, HttpServletResponse response) {
+    String sayHello(HttpServletRequest request, HttpServletResponse response) {
         try {
             if (authService.validate(request)) {
-                return new RequestResult("Success");
+                return "oauth_token=c5a8b2ff2a20524381083b1fe172fdc1&oauth_token_secret=8508e7c450fc2462ae4932fa63c35b30&oauth_callback_confirmed=true";
             }
         } catch (AuthException e) {
-            return new RequestResult("Failed " + e.getCause());
+            return "fucked up";
         }
-        return new RequestResult("Failed");
+        return "failed";
     }
 
 }
